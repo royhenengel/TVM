a82198b677e9df756f03ba0b31d5280afabf815c
Roy Engel <Roy.Engel@priceline.com>
2020-02-06 12:55:55 -0500

    Change secondary fragment to a dagger fragment

--- ui/src/main/java/com/example/tvm/ui/secondary/view/SecondaryFragment.kt
+++ ui/src/main/java/com/example/tvm/ui/secondary/view/SecondaryFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.tvm.ui.databinding.SecondaryFragmentBinding
 import com.example.tvm.ui.secondary.viewmodel.SecondaryFragmentViewModel
+import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
-class SecondaryFragment : Fragment() {
+class SecondaryFragment : DaggerFragment() {
 
     companion object {
         fun newInstance() = SecondaryFragment()
